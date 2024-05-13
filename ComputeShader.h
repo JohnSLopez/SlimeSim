1e1e4a4700ac4cdf26b8740e26a150718a101e41
Ricketysole <Ricketysole@gmail.com>
2024-05-13 18:55:50 -0500

    Set up compute shader to use for slime simulation. Currently colors pixel based on location of screen

---
+++ ComputeShader.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class ComputeShader
+{
+public:
+	unsigned int ID;
+
+	ComputeShader(const char* computePath)
+	{
+		std::string cShaderCode;
+		std::ifstream cShaderFile;
+
+		cShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+		try
+		{
+			cShaderFile.open(computePath);
+
+			std::stringstream cShaderStream;
+			cShaderStream << cShaderFile.rdbuf();
+
+			cShaderFile.close();
+			cShaderCode = cShaderStream.str();
+		}
+		catch (std::ifstream::failure& e)
+		{
+			std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ: " << e.what() << std::endl;
+		}
+
+		const char* shaderCode = cShaderCode.c_str();
+
+		unsigned int compute;
+
+		compute = glCreateShader(GL_COMPUTE_SHADER);
+		glShaderSource(compute, 1, &shaderCode, NULL);
+		glCompileShader(compute);
+		checkCompileErrors(compute, "COMPUTE");
+
+		//Shader Program
+		ID = glCreateProgram();
+		glAttachShader(ID, compute);
+		glLinkProgram(ID);
+		checkCompileErrors(ID, "PROGRAM");
+
+		glDeleteShader(compute);
+	}
+
+	void use()
+	{
+		glUseProgram(ID);
+	}
+
+private:
+	void checkCompileErrors(unsigned int shader, std::string type)
+	{
+		int success;
+		char infoLog[1024];
+		if (type != "PROGRAM")
+		{
+			glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+			if (!success)
+			{
+				glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+				std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << std::endl << infoLog << std::endl << "-------------------------------------------";
+			}
+		}
+		else
+		{
+			glGetProgramiv(shader, GL_LINK_STATUS, &success);
+			if (!success)
+			{
+				glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+				std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << std::endl << infoLog << std::endl << "-------------------------------------------";
+			}
+		}
+	}
+};
--- Source.cpp
+++ Source.cpp
@@ -3,6 +3,7 @@
 #include "VertexBuffer.h"
 #include <iostream>
 #include "Shader.h"
+#include "ComputeShader.h"
 
 int windowWidth = 1920;
 int windowHeight = 1080;
@@ -20,7 +21,6 @@ struct Agent
 	};
 	position agentPosition;
 
-	//may need to clamp (0,360)
 	float direction = 0.0f;
 
 	Agent(float x, float y, float inputDirection) 
@@ -42,7 +42,7 @@ int main()
 {
 	//Initialize glfw
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
@@ -74,14 +74,31 @@ int main()
 	VertexBuffer agentBuffer(agents, sizeof(agents));
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	//
-	//deltaTime = glfwGetTime();
-	//VertexBuffer timeBuffer(&deltaTime, sizeof(double));
-	//glVertexAttribPointer(1, 1, GL_FLOAT, GL_FALSE, sizeof(double), (void*)0);
-	//glEnableVertexAttribArray(1);
 
-	//Create main shader
-	Shader slimeShader("slimeShader.vert", "slimeShader.frag");
+	//Create shaders
+	Shader vfSlimeShader("slimeShader.vert", "slimeShader.frag");
+	ComputeShader cSlimeShader("slimeShader.comp");
+
+	vfSlimeShader.use();
+	vfSlimeShader.setInt("tex", 0);
+
+	//Create Textures
+	const unsigned int TEXTURE_WIDTH = 1920, TEXTURE_HEIGHT = 1080;
+	unsigned int texture;
+
+	glGenTextures(1, &texture);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, TEXTURE_WIDTH, TEXTURE_HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
+
+	glBindImageTexture(0, texture, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
 
 	glPointSize(10);
 	glBindVertexArray(0);
@@ -89,26 +106,21 @@ int main()
 	//main render loop
 	while (!glfwWindowShouldClose(window))
 	{
-		float time = glfwGetTime();
-		std::cout << time << "\n";
 		//input
 		processInput(window);
 
 		//rendering commands here
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+		cSlimeShader.use();
+		glDispatchCompute((unsigned int)TEXTURE_WIDTH, (unsigned int)TEXTURE_HEIGHT, 1);
+		glMemoryBarrier(GL_ALL_BARRIER_BITS);
+
 		glClear(GL_COLOR_BUFFER_BIT);
-
-		//timeBuffer.Bind();
-		//glBufferData(1, sizeof(float), &time, GL_DYNAMIC_DRAW);
-
-		int uniformLocation = glGetUniformLocation(slimeShader.ID, "time");
-		slimeShader.use();
-		glUniform1f(uniformLocation, time);
-
-		//agentBuffer.Bind();
-		slimeShader.use();
+		vfSlimeShader.use();
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_POINTS, 0, 3);
+
 		//call events and swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
